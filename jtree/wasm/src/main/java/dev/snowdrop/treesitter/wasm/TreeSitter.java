c37d8c51b044004a762003647c9e82e2d95a0713
Charles Moulliard <cmoulliard@ibm.com>
2026-06-08 15:54:16 +0200

    Add chichory BOM. Refactor to avoid the errors like: unknown import, could not find host function for import number
    Signed-off-by: Charles Moulliard <cmoulliard@ibm.com>

--- jtree/wasm/src/main/java/dev/snowdrop/treesitter/wasm/TreeSitter.java
+++ jtree/wasm/src/main/java/dev/snowdrop/treesitter/wasm/TreeSitter.java
@@ -4,21 +4,24 @@ import com.dylibso.chicory.runtime.ImportValues;
 import com.dylibso.chicory.runtime.Instance;
 import com.dylibso.chicory.wasi.WasiOptions;
 import com.dylibso.chicory.wasi.WasiPreview1;
+import com.dylibso.chicory.wasm.types.MemoryLimits;
 
 public final class TreeSitter implements AutoCloseable {
     private final Instance instance;
     private final WasiPreview1 wasi;
     private final TreeSitter_ModuleExports exports;
 
-    private TreeSitter() {
+    private TreeSitter(MemoryLimits memoryLimits) {
         var wasiOpts = WasiOptions.builder().build();
         this.wasi = WasiPreview1.builder().withOptions(wasiOpts).build();
         var imports = ImportValues.builder().addFunction(wasi.toHostFunctions()).build();
-        this.instance =
-                Instance.builder(TreeSitterModule.load())
+        var builder = Instance.builder(TreeSitterModule.load())
                         .withImportValues(imports)
-                        .withMachineFactory(TreeSitterModule::create)
-                        .build();
+                        .withMachineFactory(TreeSitterModule::create);
+        if (memoryLimits != null) {
+            builder.withMemoryLimits(memoryLimits);
+        }
+        this.instance = builder.build();
         this.exports = new TreeSitter_ModuleExports(this.instance);
     }
 
@@ -42,10 +45,17 @@ public final class TreeSitter implements AutoCloseable {
     }
 
     public static final class Builder {
+        private MemoryLimits memoryLimits;
+
         private Builder() {}
 
+        public Builder withMemoryLimits(MemoryLimits memoryLimits) {
+            this.memoryLimits = memoryLimits;
+            return this;
+        }
+
         public TreeSitter build() {
-            return new TreeSitter();
+            return new TreeSitter(memoryLimits);
         }
     }
 }
