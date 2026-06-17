a07c0530a7675a98e2a39c99286d51605622519a
Charles Moulliard <cmoulliard@ibm.com>
2026-06-17 15:00:15 +0200

    Deleted the examples/
    Signed-off-by: Charles Moulliard <cmoulliard@ibm.com>

--- examples/spring-boot-todo-app/src/main/java/com/todo/app/repository/TaskRepository.java
+++
@@ -1,8 +0,0 @@
-package com.todo.app.repository;
-
-import com.todo.app.entity.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-
-}
