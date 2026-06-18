dcd1df4a57435576d528da9c6b966b3bdc91823c
Charles Moulliard <cmoulliard@ibm.com>
2026-06-18 09:39:05 +0200

    Review the S-expression to match either: version, scope ot optional
    Signed-off-by: Charles Moulliard <cmoulliard@ibm.com>

--- skills/treesitter-query/examples/xml/dependency.scm
+++ skills/treesitter-query/examples/xml/dependency.scm
@@ -1,6 +1,6 @@
 ;
 (element
-  (STag . (Name) @dependency (#match? @dependency "(dependency|parent)"))
+  (STag . (Name) @tag.dep (#match? @tag.dep "(dependency|parent)"))
   (content
     (element
       (STag . (Name) @tag.g (#eq? @tag.g "groupId"))
@@ -8,7 +8,16 @@
     (element
       (STag . (Name) @tag.a (#eq? @tag.a "artifactId"))
       (content . (CharData) @artifactId))
-    (element
-      (STag . (Name) @tag.o (#match? @tag.o "(version|scope|optional)"))
-      (content . (CharData) @version))?
-  )) @dependency.block
+    [
+      (element
+        (STag . (Name) @tag.v (#eq? @tag.v "version"))
+        (content . (CharData) @version))?
+      (element
+        (STag . (Name) @tag.s (#eq? @tag.s "scope"))
+        (content . (CharData) @scope))?
+      (element
+        (STag . (Name) @tag.o (#eq? @tag.o "optional"))
+        (content . (CharData) @optional))?
+      ]
+  )
+) @dependency.block
