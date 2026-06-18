91b6f3afb0931da3f7c1e9bfb836129588054b96
Charles Moulliard <cmoulliard@ibm.com>
2026-06-18 09:11:30 +0200

    Add new tag to look at: scope and optional
    Signed-off-by: Charles Moulliard <cmoulliard@ibm.com>

--- skills/treesitter-query/examples/xml/dependency.scm
+++ skills/treesitter-query/examples/xml/dependency.scm
@@ -1,11 +1,14 @@
 ;
 (element
-  (STag . (Name) @tag.dep (#eq? @tag.dep "dependency"))
+  (STag . (Name) @dependency (#match? @dependency "(dependency|parent)"))
   (content
     (element
       (STag . (Name) @tag.g (#eq? @tag.g "groupId"))
-      (content . (CharData) @group.id))
+      (content . (CharData) @groupId))
     (element
       (STag . (Name) @tag.a (#eq? @tag.a "artifactId"))
-      (content . (CharData) @artifact.id))
+      (content . (CharData) @artifactId))
+    (element
+      (STag . (Name) @tag.o (#match? @tag.o "(version|scope|optional)"))
+      (content . (CharData) @version))?
   )) @dependency.block
